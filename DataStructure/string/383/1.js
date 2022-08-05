4111d5e54adc4149d2470b38f0595c435ed03f74
suyeon woo <woosean999@gmail.com>
2022-08-05 09:36:16 +0900

    DataStructure / Day6 Ransom Note / string

---
+++ DataStructure/string/383/1.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const map = new Map();
+  for (let i = 0; i < magazine.length; ++i) {
+    if (!map.get(magazine[i])) map.set(magazine[i], 1);
+    else map.set(magazine[i], map.get(magazine[i]) + 1);
+  }
+
+  for (let i = 0; i < ransomNote.length; ++i) {
+    if (!map.get(ransomNote[i])) return false;
+    else map.set(ransomNote[i], map.get(ransomNote[i]) - 1);
+  }
+
+  return true;
+};
+
+// starts at 09:28
+// submits at 09:34
+// time : 6
