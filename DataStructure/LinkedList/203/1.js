139a9dbc11f42bc1dd0387089bcc1528314e7ff6
suyeon woo <woosean999@gmail.com>
2022-08-10 18:08:44 +0900

    203 - 주석 추가

--- DataStructure/LinkedList/203/1.js
+++ DataStructure/LinkedList/203/1.js
@@ -16,24 +16,23 @@ var removeElements = function (head, val) {
     return head;
   }
   let current, prev;
-  current = head;
+  current = head; // head(원본) 남기기 위해 current변수에 담기
 
   while (current) {
     if (head.val === val) {
       head = head.next;
-      current = head;
+      current = head; // head대신 current를 이동
     } else {
-      if (current.next === null && current.val === val) {
-        prev.next = current;
-        return head;
-      } else if (current.next === null) {
-        return head;
+      if (current.next === null) {
+        return head; // current탐색 끝났으면 head리턴
       } else {
-        prev = current;
-        current = current.next;
+        prev = current; // prev 에 current 노드를 담고,
+        current = current.next; // current에 다음 노드를 담는다.
+
         if (current.val === val) {
-          prev.next = current.next;
-          current = prev;
+          // 만약 다음 노드의 데이터가 삭제해야 하는 데이터라면
+          prev.next = current.next; // prev에 그 다음노드를 연결하고,
+          current = prev; // current에 prev노드를 담는다.
         }
       }
     }
